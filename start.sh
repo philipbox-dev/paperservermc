f0f75700bdd78fdddaca223e06b13b97a0f47186
Philipbox <101444980+philipboxfrmoscow@users.noreply.github.com>
2023-07-06 07:58:13 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 
-/usr/lib/jvm/java-20-openjdk/bin/java -Xmx7G -Xms2G -jar /home/philip/Projects/mservers/mserverfrends4/paper.jar &
+java -jar -Xmx7G -Xms2G -jar ./paper.jar &
 sleep 1
 
